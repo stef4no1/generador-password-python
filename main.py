70a21e4bbd066a06989b1bda753010140d60da92
Stefano Straga <stefanostraga2k18@gmail.com>
2022-09-09 19:50:56 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random;
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Bienvenido al generador de contraseñas!")
+nr_letters = int(input(f"Cuantas letras queres que tenga tu contraseña?\n"))
+nr_symbols = int(input(f"Cuantos simbolos queres que tenga tu contraseña?\n"))
+nr_numbers = int(input(f"Cuantos numeros queres que tenga tu contraseña?\n"))
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Tu contraseña es: {password}")
